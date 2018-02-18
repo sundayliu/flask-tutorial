885de47e4eb95adaf7be7a8b231cd2365c0403e1
sundayliu <martinkro@aliyun.com>
2018-02-18 13:30:59 +0800

    errorhandler

--- hello.py
+++ hello.py
@@ -17,6 +17,14 @@ def index():
 def user(name):
     return render_template('user.html',name=name)
     
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+    
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 505
+    
 if __name__ == '__main__':
     #print(__name__)
     app.run(debug=True)
