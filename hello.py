00bc7057ba38f0ca8c0e9b2615aa1710cf74e8af
sundayliu <martinkro@aliyun.com>
2018-02-19 10:11:02 +0800

    WTForms

--- hello.py
+++ hello.py
@@ -4,14 +4,31 @@ from flask import Flask,render_template
 from flask import request
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
+
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('What is your name?',validators=[Required()])
+    submit = SubmitField('Submit')
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html',form=form,name=name)
     
 @app.route('/user/<name>')
 def user(name):
